6b432d5779de057ee0536b91a63ee287b9ddc8f4
Michel Alves <michelasm3@gmail.com>
2024-11-25 18:31:41 -0300

    Create base for api

--- src/pages/api/checkout.tsx
+++ src/pages/api/checkout.tsx
@@ -2,6 +2,30 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default function checkout(req: NextApiRequest, res: NextApiResponse){
 
-    
+    if(req.method === "POST"){
+        const headers = req.headers;
+        if(headers["x-key"]){
+            try {
+
+                const key = headers["x-key"].toString().split("Bearer ")[1];
+                if(key === process.env.STRIPE_API){
+
+                    
+
+                }
+
+                return res.status(401).json({erro:"Inválido!"});
+
+            } catch (error) {
+                return res.status(401).json({erro:"Inválido!"});
+            }
+
+        }
+
+        return res.status(401).json({erro:"Inválido!"});
+
+    }else{
+        return res.status(405).json({erro:"Inválido!"});
+    }
 
 }
